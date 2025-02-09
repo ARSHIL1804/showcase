758977cf955a460370105950a3e45b82634a6509
ARSHIL1804 <arshilvahora1804@gmail.com>
2025-02-09 12:26:54 +0530

    - project backend setup

---
+++ showcase-backend/src/main/java/com/example/showcase_backend/ShowcaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.showcase_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseBackendApplication.class, args);
+	}
+
+}
---
+++ showcase-backend/src/test/java/com/example/showcase_backend/ShowcaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showcase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
